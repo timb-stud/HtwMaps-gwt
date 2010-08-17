c85fe157909f8130884b5240928517af17ac5d92
Tim Bartsch <timb.stud@gmail.com>
2010-08-17 22:07:16 +0200

    FindPathService an neuen Connector angepasst.

--- src/de/htwmaps/client/FindPathService.java
+++ src/de/htwmaps/client/FindPathService.java
@@ -5,22 +5,23 @@ import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 import de.htwmaps.shared.PathData;
+import de.htwmaps.shared.exceptions.MySQLException;
 import de.htwmaps.shared.exceptions.NodeNotFoundException;
 import de.htwmaps.shared.exceptions.PathNotFoundException;
 import de.htwmaps.shared.exceptions.SQLException;
 @RemoteServiceRelativePath("findPath")
 public interface FindPathService extends RemoteService {
 	PathData findShortestPathAStar(String startCity, String startStreet,
-			String destCity, String destStreet) throws NodeNotFoundException, PathNotFoundException, SQLException;
+			String destCity, String destStreet) throws NodeNotFoundException, PathNotFoundException, SQLException, MySQLException;
 
 	PathData findFastestPathAStar(String startCity, String startStreet,
 			String destCity, String destStreet, int motorwaySpeed,
-			int primarySpeed, int residentialSpeed) throws NodeNotFoundException, PathNotFoundException, SQLException;
+			int primarySpeed, int residentialSpeed) throws NodeNotFoundException, PathNotFoundException, SQLException, MySQLException;
 
 	PathData findShortestPathAStarBi(String startCity, String startStreet,
-			String destCity, String destStreet) throws NodeNotFoundException, PathNotFoundException, SQLException;
+			String destCity, String destStreet) throws NodeNotFoundException, PathNotFoundException, SQLException, MySQLException;
 
 	PathData findFastestPathAStarBi(String startCity, String startStreet,
 			String destCity, String destStreet, int motorwaySpeed,
-			int primarySpeed, int residentialSpeed) throws NodeNotFoundException, PathNotFoundException, SQLException;
+			int primarySpeed, int residentialSpeed) throws NodeNotFoundException, PathNotFoundException, SQLException, MySQLException;
 }
