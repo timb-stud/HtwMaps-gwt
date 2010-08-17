0871a6433973f7bec17c9a1191e8a93e0054c96f
Tim Bartsch <timb.stud@gmail.com>
2010-08-17 22:00:14 +0200

    DBConnector vereinfacht und Singleton entfernt.

--- src/de/htwmaps/server/db/DBConnector.java
+++ src/de/htwmaps/server/db/DBConnector.java
@@ -4,77 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import de.htwmaps.shared.exceptions.MySQLException;
+
 /**
  * Name: <code>MySQL</code>
  * <p>
- * Klasse dient zum connecten zu einer Datenbank.
- * Beinhaltet ebenfalls Methoden um SQL-Querys auszufuehren
+ * Klasse gibt eine SQL Connection zurueck.
  * </p>
  * <ul>
- * 	 <li>@author YC</li>
- * 	 <li>@coauthor CR</li>
- * 	 <li>@version: 1.1 - 08.06.2010</li>
+ * 	 <li>@author Yassir Klos, Christian Rech, Tim Bartsch</li>
  * </ul>
  */
 public class DBConnector {
 
-	private String host;
-	private String username;
-	private String password;
-	private String driver;
-	private static boolean connected = false;
-	private Connection con = null;
-	private static DBConnector instance = null;
+	private static final String host = DBProperties.getString("DBConnector.host");
+	private static final String username = DBProperties.getString("DBConnector.username");
+	private static final String password = DBProperties.getString("DBConnector.password");
+	private static final String driver = DBProperties.getString("DBConnector.driver");
 
-	private DBConnector() {
-		driver = DBProperties.getString("DBConnector.driver"); //$NON-NLS-1$
-		host = DBProperties.getString("DBConnector.host"); //$NON-NLS-1$
-		password = DBProperties.getString("DBConnector.password"); //$NON-NLS-1$
-		username = DBProperties.getString("DBConnector.username"); //$NON-NLS-1$
-		connected = connect();
-	}
-
-	protected void finalize() {
-		connected = disconnect();
-		instance = null;
-	}
-
-	public static boolean isConnected() {
-		return connected;
-	}
-
-	public static Connection getConnection(){
-		if (instance == null) {
-			instance = new DBConnector();
+	public static Connection getConnection() throws SQLException, MySQLException{
+		try {
+			Class.forName(driver).newInstance();
+			return DriverManager.getConnection(host, username, password);
+		} catch (InstantiationException e) {
+			throw new MySQLException(e);
+		} catch (IllegalAccessException e) {
+			throw new MySQLException(e);
+		} catch (ClassNotFoundException e) {
+			throw new MySQLException(e);
 		}
-		return instance.con;
-	}
-
-	private boolean connect() {
-			try {
-				Class.forName(driver).newInstance();
-				con = DriverManager.getConnection(host, username, password);
-
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		return true;
 	}
 	
-	private boolean disconnect() {
-		if (connected) {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return false;
-	}
 }
