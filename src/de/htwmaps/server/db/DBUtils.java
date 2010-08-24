46e7c66882241d6c6876780b029e511ed54e3192
stas-bob <oot.pac@googlemail.com>
2010-08-24 23:21:07 +0200

    dbutils unntige if entfernt

--- src/de/htwmaps/server/db/DBUtils.java
+++ src/de/htwmaps/server/db/DBUtils.java
@@ -119,8 +119,6 @@ public class DBUtils {
 					inOrder = false;
 				}
 			}
-			if(edges[i] == null) 
-				continue;
 			myEdgeID = edges[i].getID();
 			ps1.setInt(1, myEdgeID);
 			ps2.setInt(1, myEdgeID);
