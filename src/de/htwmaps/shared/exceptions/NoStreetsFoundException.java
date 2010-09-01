5bdef480306aadb3e33937ea4f5dd3523d5e37af
Tim Bartsch <timb.stud@gmail.com>
2010-09-01 12:28:25 +0200

    TODOs aus AuotGenerated Exceptions entfernt.

--- src/de/htwmaps/shared/exceptions/NoStreetsFoundException.java
+++ src/de/htwmaps/shared/exceptions/NoStreetsFoundException.java
@@ -5,22 +5,18 @@ public class NoStreetsFoundException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public NoStreetsFoundException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public NoStreetsFoundException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NoStreetsFoundException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public NoStreetsFoundException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 }
