fd9b4bf2138a3465c935a85b1641c9e75d6d57f9
Tim Bartsch <timb.stud@gmail.com>
2010-09-08 16:11:58 +0200

    getSpeed gibbt nun vorgeschlagene speeds zurueck

--- src/de/htwmaps/server/algorithm/AStarEdge.java
+++ src/de/htwmaps/server/algorithm/AStarEdge.java
@@ -1,13 +1,25 @@
 package de.htwmaps.server.algorithm;
 
-
-
 /**
  * 
  * @author Stanislaw Tartakowski, Tim Bartsch
  *	Diese Klasse ist eine Kante zwischen 2 Knoten im Graphen
  */
 public class AStarEdge {
+	public static final int MOTORWAY_ID 		= 1;
+	public static final int PRIMARY_ID 			= 5;
+	public static final int SECONDARY_ID 		= 7;
+	public static final int RESIDENTIAL_ID 		= 10;
+	public static final int ROAD_ID 			= 11;
+	public static final int LIVING_STREET_ID 	= 13;
+	
+	public static final int MOTORWAY_SPEED 		= 100; 	//Autobahn
+	public static final int PRIMARY_SPEED 		= 70; 	//Landstraﬂe
+	public static final int SECONDARY_SPEED 	= 60; 	//Ortsverbindung
+	public static final int RESIDENTIAL_SPEED 	= 40; 	//Innerorts
+	public static final int ROAD_SPEED 			= 50; 	//unclassified
+	public static final int LIVING_STREET_SPEED = 5; 	//Spielstrasse
+	
 	private Node successor;
 	private double length;
 	private int highwayType;
@@ -31,7 +43,26 @@ public class AStarEdge {
 	}
 	
 	public int getSpeed() {
-		return speed;
+		if(speed == 1){
+			switch (highwayType) {
+			case MOTORWAY_ID:
+				return MOTORWAY_SPEED;
+			case PRIMARY_ID:
+				return PRIMARY_SPEED;
+			case SECONDARY_ID:
+				return SECONDARY_SPEED;
+			case RESIDENTIAL_ID:
+				return RESIDENTIAL_SPEED;
+			case ROAD_ID:
+				return ROAD_SPEED;
+			case LIVING_STREET_ID:
+				return LIVING_STREET_SPEED;
+			default:
+				throw new RuntimeException("highwayType: " + highwayType + " is no defined ID");
+			}
+		}
+		else
+			return speed;
 	}
 	
 	public int getWayID() {
